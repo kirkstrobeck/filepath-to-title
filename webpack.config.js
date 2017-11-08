ac82fc506d10b13066113c5341645b48d206d64e
Kirk Strobeck <kirk@strobeck.com>
2017-11-08 05:44:14 -0800

    Prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,40 @@
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: './lib/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+const { NODE_ENV } = process.env;
+const isProd = NODE_ENV === 'production';
+
+module.exports = Object.assign(
+  {
+    entry: './lib/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
+      ],
+    },
+    output: {
+      filename: './dist/index.js',
+      libraryTarget: 'umd',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  },
+  isProd && {
+    devtool: 'cheap-module-source-map',
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify(NODE_ENV),
+        },
+      }),
     ],
-  },
-  output: {
-    filename: './dist/index.js',
-    libraryTarget: 'umd',
-  },
-  plugins: [new UglifyJSPlugin()],
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-};
+  }
+);
